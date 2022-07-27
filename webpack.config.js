e71bfd71dd72793e2a503047a81d24517769d7a0
PWiernasz <100784603+PWiernasz@users.noreply.github.com>
2022-07-27 15:24:19 -0400

    added webpack analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './assets/js/script.js',
@@ -9,9 +10,12 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
+      $: "jquery",
+      jQuery: "jquery"
     }),
-  ],
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    })
+  ],    
   mode: 'development'
 };
